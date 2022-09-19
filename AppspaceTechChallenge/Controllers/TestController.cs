244d105dc0f1cc2231c670424e3e8a4a39ad84c0
sergicobo <sergicl.4407@gmail.com>
2022-09-19 11:54:09 +0200

    chore: Initial Api setup and test controller

---
+++ AppspaceTechChallenge/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppspaceTechChallenge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+
+        public TestController()
+        {
+        }
+
+        [HttpGet]
+        public OkObjectResult Get()
+        {
+            return Ok("This is a test");
+        }
+    }
+}
