8807045a24149fc97171d219006bbe11df159cd6
sergicobo <sergicl.4407@gmail.com>
2022-09-19 14:36:40 +0200

    feat: Add movies all-time recomendations

---
+++ AppspaceTechChallenge.Domain/Entities/Recommendation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppspaceTechChallenge.Domain.Entities
+{
+    public abstract class Recommendation
+    {
+        public string Title { get; set; }
+        public string Overview { get; set; }
+        public List<string> Genre { get; set; }
+        public string Language { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string WebSite { get; set; }
+        public List<string> AssociatedKeywords { get; set; }
+    }
+}
---
+++ AppspaceTechChallenge/Controllers/Viewers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using AppspaceTechChallenge.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace AppspaceTechChallenge.API.Controllers.Viewers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        [HttpGet("all-time")]
+        [ProducesResponseType(typeof(Movie), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetAllTimeRecommendations([FromQuery] List<string> keywords, [FromQuery] List<string> genres)
+        {
+            return NotFound("... Coming Soon ...");
+        }
+    }
+}
