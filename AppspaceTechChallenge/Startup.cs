cadd00bfcb355e495743fef25a92510121298762
sergicobo <sergicl.4407@gmail.com>
2022-09-19 13:16:02 +0200

    chore: Add Swagger

--- AppspaceTechChallenge/Startup.cs
+++ AppspaceTechChallenge/Startup.cs
@@ -8,17 +8,21 @@ namespace AppspaceTechChallenge
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +42,14 @@ namespace AppspaceTechChallenge
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Appspace Tech Challenge v1");
+                options.RoutePrefix = string.Empty;
             });
         }
     }
